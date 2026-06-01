3996b15d50c011ab84e3569d3161eab9266670dd
Max Biber <165724431+MaxBiber@users.noreply.github.com>
2026-06-01 23:43:12 +0200

    Add quality checks script for silver layer data
    This script performs quality checks for data consistency, accuracy, and standardization in the silver layer. It includes checks for null or duplicate primary keys, unwanted spaces, data standardization, invalid date ranges, and consistency between related fields.

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'silver.crm_cust_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT
+cst_id,
+COUNT(*)
+FROM [bronze].[crm_cust_info]
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
