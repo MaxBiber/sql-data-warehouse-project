de26804c44ec3f1b61eb8cfd193c3e5db576fdc7
Max Biber <165724431+MaxBiber@users.noreply.github.com>
2026-06-28 00:20:54 +0200

    Fix formatting and structure in ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -34,18 +34,22 @@ CREATE TABLE silver.crm_sales_details (
 	sls_price INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
-	DROP TABLE silver.crm_prd_info;
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+
 CREATE TABLE silver.crm_prd_info (
-	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
