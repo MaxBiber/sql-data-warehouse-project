b04d48ef6747e2a5124df0eeac56fa46fa581038
Max Biber <165724431+MaxBiber@users.noreply.github.com>
2026-06-28 00:23:12 +0200

    Fix SQL syntax and update table definitions

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -20,20 +20,24 @@ CREATE TABLE silver.crm_cust_info (
 	cst_create_date DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
-	DROP TABLE silver.crm_sales_details;
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
 CREATE TABLE silver.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
+
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
 GO
